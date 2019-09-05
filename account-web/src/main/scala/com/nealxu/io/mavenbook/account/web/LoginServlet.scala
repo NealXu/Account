a5556ae9f518b5ed845d3a1c73dca3820cd1d288
nealxu <xuqixq@sina.com>
2019-09-05 21:58:53 +0800

    delete println

--- account-web/src/main/scala/com/nealxu/io/mavenbook/account/web/LoginServlet.scala
+++ account-web/src/main/scala/com/nealxu/io/mavenbook/account/web/LoginServlet.scala
@@ -26,25 +26,18 @@ class LoginServlet extends HttpServlet {
     val id = req.getParameter("id")
     val password = req.getParameter("password")
 
-    println(s"id=$id  password=$password")
 
     if (id == null || id.length == 0 || password == null || password.length == 0) {
-      println("incomplete parameter")
       resp.sendError(400, "incomplete parameter")
     } else {
 
       try {
         val service = context.getBean("accountService").asInstanceOf[AccountService]
 
-        println(s"context is null(${context == null})")
-        println(s"service is null(${service == null})")
-
         service.login(id, password)
-        println("Login Successful!")
         resp.getWriter.write("Login Successful!")
       } catch {
         case e: AccountServiceException =>
-          println(e.getMessage)
           resp.sendError(400, e.getMessage)
       }
     }
